9c8f939db0c04b4016a43b598e68bb60a273ba9b
Ibrahim Anifowoshe <ibrahim.anifowoshe.uk@gmail.com>
2023-03-27 01:34:06 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+});
+
+
+async function submitForm(event) {
+  event.preventDefault();
+
+  const form = document.getElementById('contact-form');
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch('/send-email', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      form.style.display = 'none';
+      document.getElementById('success-message').style.display = 'block';
+    } else {
+      // Handle error case
+    }
+  } catch (error) {
+    // Handle error case
+  }
+}
