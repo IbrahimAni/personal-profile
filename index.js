bd1d5962adf323d1e18115be0f8a4f7b755c2637
Ibrahim Anifowoshe <ibrahim.anifowoshe.uk@gmail.com>
2023-04-02 17:01:04 +0100

    added cloud enterprice project link to projects done

--- index.js
+++ index.js
@@ -46,3 +46,57 @@ document.addEventListener("DOMContentLoaded", function () {
 //     // Handle error case
 //   }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showAlert(action, type) {
+//   var backgroundColor;
+//   var message;
+
+//   if (action === "added") {
+//       backgroundColor = "bg-green-500";
+//       message = "The book has been added.";
+//   } else if (action === "updated") {
+//       backgroundColor = "bg-blue-500";
+//       message = "The book has been updated.";
+//   } else if (action === "deleted") {
+//       backgroundColor = "bg-red-500";
+//       message = "The book has been deleted.";
+//   } else {
+//       backgroundColor = "bg-gray-500";
+//       message = "An unknown action was performed.";
+//   }
+
+//   var alertHTML = `
+//       <div class="fixed top-0 left-0 right-0 z-10">
+//           <div class="${backgroundColor} text-white px-4 py-3 shadow-md" role="alert">
+//               <p class="font-bold">${type == "error" ? "Error!" : "Success!"}</p>
+//               <p class="text-sm">${message}</p>
+//           </div>
+//       </div>
+//   `;
+
+//   var alertElement = document.getElementById("alert");
+//   alertElement.innerHTML = alertHTML;
+//   alertElement.classList.remove("hidden");
+
+//   setTimeout(function () {
+//       alertElement.classList.add("hidden");
+//       alertElement.innerHTML = '';
+//   }, 3000);
+// }
